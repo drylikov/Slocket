d779de3254314c7131955f14747486bfe1aa2049
drylikov <denis.rylikov@protonmail.com>
2025-02-05 00:01:42 +0300

    Slocket
    A locking socket alternative to file-system mutex locks.

---
+++ index.js
@@ -0,0 +1,320 @@
+module.exports = Slocket
+
+var rimraf = require('rimraf')
+var assert = require('assert')
+var EE = require('events')
+var net = require('net')
+var fs = require('fs')
+var path = require('path')
+var onExit = require('signal-exit')
+var locks = Object.create(null)
+var ID = 0
+
+/* istanbul ignore if */
+if (typeof Promise === undefined)
+  Promise = require('bluebird')
+
+var util = require('util')
+util.inherits(Slocket, EE)
+
+var debug = function () {}
+
+/* istanbul ignore if */
+if (/\bslocket\b/i.test(process.env.NODE_DEBUG || '')) {
+  debug = function () {
+    var msg = util.format.apply(util, arguments)
+    var n = path.basename(this.name)
+    var p = 'SLOCKET:' + process.pid + ':' + n + ':' + this.id + ' '
+    msg = p + msg.trimRight().split('\n').join('\n' + p)
+    console.error(msg)
+  }
+}
+
+function Slocket (name, cb) {
+  if (!(this instanceof Slocket))
+    return new Slocket(name, cb)
+
+  this.id = ID++
+  this.name = path.resolve(name)
+  /* istanbul ignore next */
+  if (process.platform === 'win32' && !/^\\\\.\\pipe\\/i.test(name))
+    this.name = '\\\\.\\pipe\\' + name
+  if (cb)
+    this.cb = cb
+  this.server = null
+  this.connection = null
+  this.watcher = null
+  this.has = false
+  this.had = false
+  this.connectionQueue = []
+  this.currentClient = null
+  this.windows = process.platform === 'win32'
+
+  this.promise = new Promise(function (resolve, reject) {
+    this.resolve = resolve
+    this.reject = reject
+  }.bind(this))
+  this.then = this.promise.then.bind(this.promise)
+  this.catch = this.promise.catch.bind(this.promise)
+  this.release = this.release.bind(this)
+
+  this.acquire()
+}
+
+Slocket.prototype.debug = debug
+
+Slocket.prototype.cb = function () {}
+
+Slocket.prototype.acquire = function () {
+  this.debug('acquire')
+  this.unwatch()
+  this.disconnect()
+  this.server = net.createServer(this.onServerConnection.bind(this))
+  this.server.once('error', this.onServerError.bind(this))
+  this.server.listen(this.name, this.onServerListen.bind(this))
+  this.server.on('close', this.onServerClose.bind(this))
+  this.server.unref()
+}
+
+Slocket.prototype.onAcquire = function () {
+  this.debug('onAcquire')
+  this.unwatch()
+  assert.equal(this.has, false)
+  this.has = true
+  this.had = true
+  this.emit('acquire')
+  this.cb(null, this)
+  // Promises are sometimes a little clever
+  // when you resolve(<Promise>), it hooks onto the .then method
+  // of the promise it's resolving to.  To avoid never actually
+  // resolving, we wrap to hide the then/catch methods.
+  this.resolve(Object.create(this, {
+    then: { value: undefined },
+    catch: { value: undefined },
+    resolve: { value: undefined },
+    reject: { value: undefined },
+    release: { value: this.release.bind(this) }
+  }))
+}
+
+Slocket.prototype.onServerListen = function () {
+  this.server.listening = true
+  this.debug('onServerListen', this.server.listening)
+  this.emit('serverListen')
+  process.nextTick(function onServerListenNT () {
+    this.debug('onServerListenNT', this.server.listening)
+    this.on('serverClose', onExit(this.onProcessExit.bind(this)))
+    this.onAcquire()
+  }.bind(this))
+}
+
+Slocket.prototype.onProcessExit = function () {
+  this.debug('onProcessExit')
+  if (this.has === true)
+    this.release(true)
+}
+
+Slocket.prototype.onServerConnection = function (c) {
+  this.debug('onServerConnection')
+  this.emit('serverConnection', c)
+  c.on('close', this.onServerConnectionClose.bind(this, c))
+
+  // Nearly impossible, but the race condition between the server
+  // closing after releasing a lock, and a client connecting can
+  // sometimes be hit in the pound test.  Ignore for test coverage.
+  /* istanbul ignore else */
+  if (this.currentClient || this.has)
+    this.connectionQueue.push(c)
+  else
+    this.delegate(c)
+}
+
+Slocket.prototype.onServerConnectionClose = function (c) {
+  this.debug('onServerConnectionClose', this.has)
+  this.emit('serverConnectionClose', c)
+  if (this.currentClient === c) {
+    this.currentClient = null
+    this.release()
+  }
+
+  var i = this.connectionQueue.indexOf(c)
+  if (i !== -1)
+    this.connectionQueue.splice(i, 1)
+}
+
+Slocket.prototype.delegate = function (c) {
+  if (this.windows)
+    return this.serverClose(c)
+
+  this.debug('delegate')
+  assert.equal(this.has, false)
+  this.debug('delegate new client')
+  this.currentClient = c
+  c.write('OK')
+}
+
+Slocket.prototype.type = function () {
+  return !this.has ? 'none'
+    : this.server && this.server.listening ? 'server'
+    : this.connection ? 'connection'
+    : 'wtf'
+}
+
+Slocket.prototype.release = function (sync) {
+  this.debug('release has=%j sync=%j', this.has, sync)
+  this.has = false
+  this.connectionRelease(sync)
+  this.serverRelease(sync)
+}
+
+Slocket.prototype.serverRelease = function (sync) {
+  if (!this.server)
+    return
+
+  this.debug('serverRelease %j', sync, this.connectionQueue.length)
+  if (this.connectionQueue.length)
+    this.delegate(this.connectionQueue.shift())
+  else
+    this.serverClose()
+}
+
+Slocket.prototype.serverClose = function (conn) {
+  this.debug('serverClose', this.connectionQueue.length)
+  this.server.listening = false
+  this.server.close()
+  if (conn)
+    conn.destroy()
+  this.connectionQueue.forEach(function (connection) {
+    connection.destroy()
+  })
+  this.connectionQueue.length = 0
+}
+
+Slocket.prototype.onServerClose = function () {
+  this.debug('onServerClose')
+  this.emit('serverClose')
+  this.server = null
+}
+
+Slocket.prototype.onServerError = function (er) {
+  this.debug('onServerError', er.message)
+  this.emit('serverError', er)
+  this.server = null
+  switch (er.code) {
+    case 'ENOTSOCK':
+      return this.watch()
+    case 'EADDRINUSE':
+    case 'EEXIST':
+      return this.connect()
+    default:
+      er.slocket = 'server'
+      this.onError(er)
+  }
+}
+
+Slocket.prototype.onError = function (er) {
+  this.debug('onError', er.message)
+  this.cb(er, this)
+  this.reject(er)
+}
+
+Slocket.prototype.connect = function () {
+  this.debug('connect')
+  this.connection = net.createConnection(this.name)
+  this.connection.slocketBuffer = ''
+  this.connection.setEncoding('utf8')
+  this.connection.slocketConnected = false
+  this.connection.on('connect', this.onConnect.bind(this))
+  this.connection.on('error', this.onConnectionError.bind(this))
+  this.connection.on('data', this.onConnectionData.bind(this))
+}
+
+Slocket.prototype.onConnectionData = function (chunk) {
+  this.debug('onConnectionData %s', chunk, this.connection.slocketBuffer)
+  this.emit('connectionData', chunk)
+  this.connection.slocketBuffer += chunk
+
+  if (this.connection.slocketBuffer === 'OK')
+    this.onAcquire()
+
+  if (this.connection.slocketBuffer.length > 2)
+    this.connection.destroy()
+}
+
+Slocket.prototype.onConnectionError = function (er) {
+  this.debug('onConnectionError', er.message)
+  this.emit('connectionError', er)
+  if (this.has)
+    return this.onError(er)
+  this.connection = null
+  switch (er.code) {
+    case 'ENOENT':
+      // socket was there, but now is gone!
+      return this.acquire()
+
+    case 'ENOTSOCK':
+      // something other than a socket is there already
+    case 'ECONNREFUSED':
+      // socket there, but not listening
+      // watch for changes, in case it's that it's not a socket
+      // if that fails, eg for "unknown system error", then just retry
+      try {
+        return this.watch()
+      } catch (er) {
+        return this.acquire()
+      }
+    default:
+      er.slocket = 'connection'
+      this.onError(er)
+  }
+}
+
+Slocket.prototype.onConnect = function () {
+  this.debug('onConnect')
+  this.emit('connect')
+  this.connection.slocketConnected = true
+  this.connection.on('close', this.onConnectionClose.bind(this))
+}
+
+Slocket.prototype.onConnectionClose = function () {
+  this.debug('onConnectionClose')
+  this.emit('connectionClose')
+  this.connection.slocketConnected = false
+  if (!this.had)
+    this.acquire()
+}
+
+Slocket.prototype.connectionRelease = function (sync) {
+  if (!this.connection)
+    return
+
+  this.debug('connectionRelease', sync)
+
+  if (this.connection.slocketConnected)
+    this.connection.destroy()
+  else if (sync)
+    rimraf.sync(this.name)
+  else
+    rimraf(this.name, function () {})
+}
+
+Slocket.prototype.disconnect = function () {
+  this.debug('disconnect')
+  if (this.connection)
+    this.connection.destroy()
+  this.connection = null
+}
+
+Slocket.prototype.watch = function () {
+  this.debug('watch')
+  this.watcher = fs.watch(this.name, { persistent: false })
+  this.watcher.on('change', this.acquire.bind(this))
+  this.watch.on('error', this.acquire.bind(this))
+}
+
+Slocket.prototype.unwatch = function () {
+  this.debug('unwatch')
+  if (this.watcher)
+    this.watcher.close()
+  this.watcher = null
+}
---
+++ test/basic.js
@@ -0,0 +1,417 @@
+var Slocket = require('../')
+var rimraf = require('rimraf')
+var node = process.execPath
+var spawn = require('child_process').spawn
+var fs = require('fs')
+var t = require('tap')
+var net = require('net')
+
+t.jobs = +process.env.JOBS || 4
+
+var windows = process.platform === 'win32'
+
+if (typeof Promise === 'undefined')
+  Promise = require('bluebird')
+
+t.teardown(function () {
+  names.forEach(function (n) {
+    clear(n)
+  })
+})
+
+var names = []
+var lockPrefix = windows ? ('\\\\.\\pipe\\' + __dirname + '\\')
+    : (__dirname + '/')
+
+function filename (n) {
+  names.push(n)
+  clear(n)
+  return lockPrefix + n
+}
+function clear (n) {
+  try { rimraf.sync(lockPrefix + n) } catch (er) {}
+}
+
+t.test('3 parallel locks', function (t) {
+  var locks = []
+  clear('3-parallel')
+  var file = filename('3-parallel')
+
+  // also tests returning a promise
+  for (var i = 0; i < 3; i++) {
+    locks[i] = Slocket(file)
+  }
+
+  setInterval(function () {
+    var h = has()
+    if (h.filter(h => h).length > 1)
+      throw new Error('double-lock: ' + JSON.stringify(h))
+  }, 1000).unref()
+
+  function has () {
+    return locks.map(function (l, i) {
+      return l.has
+    })
+  }
+
+  t.same(has(), [ false, false, false ], 'no locks acquired sync')
+
+  locks.forEach(function (l, i) {
+    l.then(acquired(i))
+  })
+
+  var got = []
+  function acquired (i) { return function (lock) {
+    t.comment('acquired %d', i)
+    t.equal(got.indexOf(i), -1, 'should not have gotten already')
+    got.push(i)
+    var expect = [false, false, true]
+    t.same(has().sort(), expect, 'should have exactly 1 lock')
+    t.isa(lock, Slocket)
+    t.test('should not look like a promise or deferred', function (t) {
+      t.equal(lock.then, undefined)
+      t.equal(lock.catch, undefined)
+      t.equal(lock.resolve, undefined)
+      t.equal(lock.reject, undefined)
+      t.end()
+    })
+    setTimeout(function () {
+      lock.release()
+    }, 100)
+  }}
+
+  Promise.all(locks).then(function (locks) {
+    setTimeout(function () {
+      t.same(has(), [false, false, false], 'no remaining locks')
+      t.same(got.sort(), [ 0, 1, 2 ], 'got all 3 locks')
+      clear('3-parallel')
+      t.end()
+    }, 100)
+  })
+})
+
+t.test('3 serial locks', function (t) {
+  clear('3-serial')
+  var file = filename('3-serial')
+  function go (i) {
+    if (i === 3)
+      return t.end()
+    Slocket(file, function (er, lock) {
+      if (er)
+        throw er
+      t.pass('got lock ' + i)
+      lock.release()
+      go(i+1)
+    })
+  }
+  go(0)
+})
+
+t.test('staggered', function (t) {
+  clear('3-staggered')
+  var file = filename('3-staggered')
+
+  var set = []
+  Slocket(file).then(function (lock) {
+    set[0] = lock
+    t.equal(lock.type(), 'server')
+    Slocket(file, function (er, lock) {
+      t.equal(lock.type(), windows ? 'server' : 'connection')
+      set[1] = lock
+
+      Slocket(file, function (er, lock) {
+        t.equal(lock.type(), windows ? 'server' : 'connection')
+        lock.release()
+        t.end()
+      })
+      setTimeout(function () {
+        lock.release()
+      }, 100)
+    }).on('connect', function () {
+      lock.release()
+    })
+  })
+})
+
+t.test('server disconnect', {
+  skip: windows ? 'skip on windows' : false
+}, function (t) {
+  var file = filename('server-disconnect')
+  var prog = require.resolve('./fixtures/server-disconnect.js')
+  var child = spawn(node, [prog, file])
+  child.stderr.pipe(process.stderr)
+  child.stdout.on('data', function () {
+    // now we know that the server has the lock
+    var didKill = false
+    setTimeout(function () {
+      child.kill('SIGHUP')
+    })
+
+    var clients = [
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock)
+    ]
+    Promise.all(clients).then(t.end)
+
+    function onLock (er, lock) {
+      var has = clients.filter(function (c) { return c.has })
+      t.equal(has.length, 1, 'always exactly one lock')
+      if (!didKill) {
+        didKill = true
+        child.kill('SIGINT')
+        setTimeout(lock.release, 100)
+      } else
+        lock.release()
+    }
+  })
+})
+
+t.test('server process graceful exit', {
+  skip: windows ? 'skip on windows' : false
+}, function (t) {
+  var file = filename('graceful-exit')
+  var prog = require.resolve('./fixtures/graceful-exit.js')
+  var child = spawn(node, [prog, file])
+  var childClosed = false
+  child.on('close', function (code, signal) {
+    childClosed = true
+    t.equal(code, 0)
+    t.equal(signal, null)
+  })
+
+  child.stderr.pipe(process.stderr)
+  child.stdout.on('data', function () {
+    // now we know that the server has the lock
+    var didKill = false
+    setTimeout(function () {
+      child.kill('SIGHUP')
+    })
+
+    var clients = [
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock)
+    ]
+    Promise.all(clients).then(function () {
+      t.ok(childClosed, 'child process exited gracefully')
+      t.end()
+    })
+
+    function onLock (er, lock) {
+      var has = clients.filter(function (c) { return c.has })
+      t.equal(has.length, 1, 'always exactly one lock')
+      setTimeout(lock.release, 100)
+    }
+  })
+})
+
+t.test('server process graceful exit without release', {
+  skip: windows ? 'skip on windows' : false
+}, function (t) {
+  var file = filename('server-disconnect-graceful')
+  var prog = require.resolve('./fixtures/exit-no-release.js')
+  var child = spawn(node, [prog, file], {
+    env: { NODE_DEBUG: 'slocket' }
+  })
+  var childClosed = false
+  child.on('close', function (code, signal) {
+    childClosed = true
+    t.equal(code, null)
+    t.equal(signal, 'SIGHUP')
+  })
+
+  var stderr = ''
+  child.stderr.on('data', function (c) {
+    stderr += c
+  })
+  child.stdout.on('data', function () {
+    // now we know that the server has the lock
+    var didKill = false
+    setTimeout(function () {
+      child.kill('SIGHUP')
+    })
+
+    var clients = [
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock)
+    ]
+    Promise.all(clients).then(function () {
+      t.ok(childClosed, 'child process exited gracefully')
+      t.match(stderr, /onProcessExit\n/, 'hit the onProcessExit fn')
+      t.end()
+    })
+
+    function onLock (er, lock) {
+      var has = clients.filter(function (c) { return c.has })
+      t.equal(has.length, 1, 'always exactly one lock')
+      setTimeout(lock.release, 100)
+    }
+  })
+})
+
+t.test('try to lock on a non-socket, auto-lock once gone', {
+  skip: windows ? 'skip on windows' : false
+}, function (t) {
+  var file = filename('not-a-socket')
+  var fs = require('fs')
+  fs.writeFileSync(file, 'not a socket\n')
+  var lock = Slocket(file, function (er, lock) {
+    lock.release()
+    t.end()
+  })
+  t.notOk(lock.has)
+  t.notOk(fs.statSync(file).isSocket())
+  rimraf(file, function () {})
+})
+
+t.test('try to lock on non-Slocket socket', {
+  skip: windows ? 'skip on windows' : false
+}, function (t) {
+  var file = filename('non-slocket')
+  var maker = require.resolve('./fixtures/abandon-socket.js')
+  spawn(node, [maker, file]).on('close', function () {
+    t.ok(fs.statSync(file).isSocket(), 'socket is there')
+    var deleted = false
+    setTimeout(function () {
+      fs.unlinkSync(file)
+      t.notOk(lock.has, 'should not have lock yet')
+      deleted = true
+    }, 100)
+    var lock = Slocket(file, function (er, lock) {
+      if (er)
+        throw er
+      t.ok(deleted, 'deleted file before lock acquired')
+      t.equal(lock.type(), 'server')
+      t.end()
+    })
+  })
+})
+
+t.test('server disconnect, connection sync end', function (t) {
+  var file = filename('server-disconnect-conn-sync-end')
+  var prog = require.resolve('./fixtures/server-disconnect.js')
+  var child = spawn(node, [prog, file])
+  child.stderr.pipe(process.stderr)
+  child.stdout.on('data', function () {
+    // now we know that the server has the lock
+    var didKill = false
+    setTimeout(function () {
+      child.kill('SIGINT')
+    }, 100)
+
+    Slocket(file, function onLock (er, lock) {
+      setTimeout(function () {
+        lock.release(true)
+        t.throws(fs.statSync.bind(fs, file))
+        t.end()
+      }, 100)
+    })
+  })
+})
+
+t.test('server kill connection abruptly', function (t) {
+  var file = filename('server-kill-abruptly')
+  Slocket(file, function (er, serverLock) {
+    if (er)
+      throw er
+    t.equal(serverLock.type(), 'server')
+
+    var clients = [
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock),
+      Slocket(file, onLock)
+    ]
+    Promise.all(clients).then(t.end)
+
+    function onLock (er, lock) {
+      var has = clients.filter(function (c) { return c.has })
+      t.equal(has.length, 1, 'always exactly one lock')
+      setTimeout(lock.release, 100)
+    }
+
+    setTimeout(function () {
+      t.equal(serverLock.currentClient, null)
+      t.ok(serverLock.has)
+      t.equal(serverLock.connectionQueue.length, 5)
+      serverLock.connectionQueue[0].destroy()
+      serverLock.connectionQueue[2].destroy()
+      serverLock.connectionQueue[4].destroy()
+      setTimeout(function () {
+        t.equal(serverLock.connectionQueue.length, 5)
+        serverLock.release()
+      }, 100)
+    }, 100)
+  })
+})
+
+t.test('verify behavior when pretending to be windows', function (t) {
+  var file = filename('windows-pretend')
+  var locks = [
+    Slocket(file, onLock),
+    Slocket(file, onLock),
+    Slocket(file, onLock),
+    Slocket(file, onLock),
+    Slocket(file, onLock)
+  ]
+
+  locks.forEach(function (l) {
+    l.windows = true
+  })
+
+  function onLock (er, lock) {
+    if (er)
+      throw er
+
+    // all locks are servers on windows, clients are just for waiting
+    t.equal(lock.type(), 'server', 'is a server')
+    var has = locks.filter(function (c) { return c.has })
+    t.equal(has.length, 1, 'always exactly one lock')
+    setTimeout(lock.release, 100)
+  }
+
+  return Promise.all(locks)
+})
+
+t.test('slocket.has() function', function (t) {
+  var file = filename('not-actually-used')
+  var s = Slocket(file)
+  t.equal(s.type(), 'none')
+  s.then(function () {
+    s.release(true)
+    s.has = true
+    // fake a wtf state
+    s.has = true
+    t.equal(s.type(), 'wtf')
+    t.end()
+  })
+})
+
+t.test('connect to non-slocket socket', function (t) {
+  var net = require('net')
+  var file = filename('not-a-slocket-server')
+  var server = net.createServer(function (conn) {
+    conn.write('O')
+    setTimeout(function () {
+      conn.write('this is not ok')
+      server.close()
+    })
+  })
+  server.listen(file, function () {
+    var s = Slocket(file)
+    t.end()
+  })
+})
+
+t.test('server object emit error after being removed')
+t.test('delete socket between EADDRINUSE and connect')
+t.test('release before connection connects')
